bb5acd5d2d14a0aeabf2bf5f36d57cca6b865170
Jacoby04 <opdah023@morris.umn.edu>
2016-01-06 16:13:06 -0600

    Added a way to add comments complete with a slight amount of spam protection. Doesn't really matter as it's all client-side anyway!

--- app/controllers/main/main.controller.js
+++ app/controllers/main/main.controller.js
@@ -7,4 +7,15 @@ angular.module('app.Controllers').controller('MainCtrl', function(){
 	
 	vm.title = "Welcome to Jacob's Web Playground!";
 	
+	vm.comments = [];
+	vm.newComment = "";
+	vm.secretComment = "";
+	
+	vm.addComment = function() {
+		if (vm.newComment != "" && vm.secretComment == "") {
+			vm.comments.push(vm.newComment);
+			vm.newComment = "";
+		}
+	};
+	
 });
