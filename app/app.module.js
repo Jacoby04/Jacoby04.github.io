71c0555570c5fe47166f514abb2a1da8f66b75c2
Jacoby04 <opdah023@morris.umn.edu>
2016-01-06 00:01:43 -0600

    Started getting modules and controllers set up with Angular. Currently having an issue with bootstrap script. Also, the Angular dependency injection doesn't seem to be working correctly at the moment.

---
+++ app/app.module.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('app.Controllers', []);
+angular.module('app', ['app.Controllers']);
+
---
+++ app/controllers/main/main.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('app.Controllers').controller('MainCtrl', function(){
+	
+	//vm = ViewModel
+	var vm = this;
+	
+	vm.title = "Testing"
+	
+});
