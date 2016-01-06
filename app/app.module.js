19c09c0f2a8ec426956cea4da9784eec2f56778d
Jacoby04 <opdah023@morris.umn.edu>
2016-01-06 14:05:07 -0600

    Started implementing routing through Angular. Not sure I have the correct CDN for Angular-Route. Still need to set up index.html to use an html template. Need to set up the html template.

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,15 @@
 'use strict';
 
 angular.module('app.Controllers', []);
-angular.module('app', ['app.Controllers']);
+angular.module('app', ['app.Controllers',
+					   'ngRoute'])
+	.config(function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'app/controllers/main/main.html',
+			controller: 'MainCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+	});
 
